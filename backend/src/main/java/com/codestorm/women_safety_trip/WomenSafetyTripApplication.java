466ff5acc070fe3d083840008b0db1f98ed5673c
Aishwarya-1006 <aishwaryamohan5555@gmail.com>
2025-09-01 22:04:11 +0530

    Added backend

---
+++ backend/src/main/java/com/codestorm/women_safety_trip/WomenSafetyTripApplication.java
@@ -0,0 +1,13 @@
+package com.codestorm.women_safety_trip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WomenSafetyTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WomenSafetyTripApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/codestorm/women_safety_trip/WomenSafetyTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestorm.women_safety_trip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WomenSafetyTripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
